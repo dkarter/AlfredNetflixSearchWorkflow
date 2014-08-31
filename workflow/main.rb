68797e0be739c563a943231d2450c6d86fc9bce9
Dorian Karter <dkarter@gmail.com>
2014-08-30 21:30:19 -0500

    First commit

--- workflow/main.rb
+++ workflow/main.rb
@@ -4,34 +4,50 @@
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
 require "bundle/bundler/setup"
 require "alfred"
-
+require 'json'
+require 'open-uri'
 
 
 
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
 
-  # add a file feedback
-  fb.add_file_item(File.expand_path "~/Applications/")
-
-  # add an arbitrary feedback
-  fb.add_item({
-    :uid      => ""                     ,
-    :title    => "Just a Test"          ,
-    :subtitle => "feedback item"        ,
-    :arg      => "A test feedback Item" ,
-    :valid    => "yes"                  ,
-  })
   
-  # add an feedback to test rescue feedback
-  fb.add_item({
-    :uid          => ""                     ,
-    :title        => "Rescue Feedback Test" ,
-    :subtitle     => "rescue feedback item" ,
-    :arg          => ""                     ,
-    :autocomplete => "failed"               ,
-    :valid        => "no"                   ,
-  })
+  results = JSON.load(open(URI::encode('http://localhost:8080/autocomplete/' + ARGV[0])))
+  resultsJson = JSON.parse(results)
+
+  titles = resultsJson['groups'].select { | g | g['type'] == 'titles' }
+
+  if resultsJson['groups'] == [] || titles.length == 0
+    puts "Cannot find titles"
+  else
+    movies = titles[0]['items'] || 'nothing found'
+    movies.each do |movie|
+      movie['title'] = "#{movie['title']} #{movie['disambiguation']}" if movie.has_key? 'disambiguation'
+      fb.add_item({
+        uid:      "#{movie['id']}",
+        title:    "#{movie['title']}",
+        subtitle: "http://www.netflix.com/WiPlayer?movieid=#{movie['id']}",
+        arg:      "" ,
+        valid:    "yes",
+      })
+      
+    end
+  end
+
+  
+      
+  
+  
+  # # add an feedback to test rescue feedback
+  # fb.add_item({
+  #   :uid          => ""                     ,
+  #   :title        => "Rescue Feedback Test" ,
+  #   :subtitle     => "rescue feedback item" ,
+  #   :arg          => ""                     ,
+  #   :autocomplete => "failed"               ,
+  #   :valid        => "no"                   ,
+  # })
 
   if ARGV[0].eql? "failed"
     alfred.with_rescue_feedback = true
